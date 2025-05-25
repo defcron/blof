0914d5c2f355197c8c19cbee97fa5c2d3f40caa8
Jeremy Carter <defcron@gptenv.com>
2025-05-25 17:45:17 -0400

    v0.0.1

--- blch.sh
+++ blch.sh
@@ -2,21 +2,67 @@
 # Generate a .blch (Binary Linear with Checksum Header) format file and output it to stdout.
 
 generate_blch_crumb() {
-    local args=${@}
+    # Read stdin into a temporary file
+    local tmpfile=$(mktemp)
+    cat > "$tmpfile"
 
-    (tar cOPS ${args} --owner=0 <(cat) | gzip -9 | xxd -b -c256 | awk -F": |  |\n" '{print $2}' | sed 's/\s//g' | tr -d '\n') 2>&1
+    # Archive that file
+    (tar --transform='s|.*/\(.*\)$|\1.blch.out|' --sort=name --owner=0 --group=0 --numeric-owner --format=gnu -cf - -C "$(dirname $tmpfile)" $@ "$(basename $tmpfile)" | gzip -9 | xxd -b -c256 | awk -F": |  |\n" '{print $2}' | sed 's/\s//g' | tr -d '\n') 2>&1
+
+    rm -f "$tmpfile"
+    return $?
 }
 
+
 blch() {
-    local BLCH_CRUMB=$(generate_blch_crumb "$@")
-    local bit0=$(echo $BLCH_CRUMB | head -c 1)
+    if [ "$1" = "-x" ] || [ "$1" = "x" ]; then
+        shift
 
-    if [ $bit0 != "0" -a $bit0 != "1" ]; then
-	echo "error: ${BLCH_CRUMB}" 1>&2
-        return 1
+	(cat | cut -d' ' -f2 | fold -w8 | while read -r byte; do printf "%02x" "$((2#$byte))"; done) | xxd -p -r | tar zxOf - $@
+        return $?
     fi
+    if [ "$1" = "-t" ] || [ "$1" = "t" ]; then
+        # Test checksum
+	shift
 
-    printf "%s %s" "$(echo "$BLCH_CRUMB" | sha256sum -z --tag | awk '{print $1 $2 $3 $4}' | tr -d '\0')" "$BLCH_CRUMB"
+	local stdin_contents=$(cat)
+        local header=$(printf "%s" "$stdin_contents" | cut -d' ' -f1)
+        local body=$(printf "%s" "$stdin_contents" | cut -d' ' -f2)
+	local stored_checksum=$(printf "%s" "$header" | cut -d\= -f2)
+        local calculated_checksum=$(printf "%s" "$body" | sha256sum -z --tag | awk '{print $1 $2 $3 $4}' | tr -d '\0' | cut -d\= -f2)
+
+	if [ "$stored_checksum" = "$calculated_checksum" ]; then
+            echo "true"
+	    echo "stored_checksum:	$stored_checksum" 1>&2
+	    echo "calculated_checksum:	$calculated_checksum" 1>&2
+	    return 0
+        else
+            echo "false"
+	    echo "stored_checksum:	$stored_checksum" 1>&2
+	    echo "calculated_checksum:	$calculated_checksum" 1>&2
+            return 255
+        fi
+    fi
+    if [ "$1" = "-c" ] || [ "$1" = "c" ]; then
+        shift
+    
+	local BLCH_CRUMB=$(generate_blch_crumb $@)
+        local bit0=$(echo $BLCH_CRUMB | head -c 1)
+
+	if [ "$bit0" != "0" -a "$bit0" != "1" ]; then
+            echo "error: ${BLCH_CRUMB}" 1>&2
+        
+	    return 1
+        fi
+
+	printf "%s %s" "$(printf '%s' "$BLCH_CRUMB" | sha256sum -z --tag | awk '{print $1 $2 $3 $4}' | tr -d '\0')" "$BLCH_CRUMB"
+        return 0
+    fi
+    
+    # Default mode
+    "$0" c $@ < <("$0" c $@ < "$0")
+
+    return $?
 }
 
 blch "$@"
--- blof.sh
+++ blof.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 # Generate a .blof (Binary Linear Object File) format file and output it to stdout.
 
+# TODO: This file is currently out-of-date and not working, but just use blch.sh instead, it's better anyway.
+# Or you can update this to work the similar way as in blch.sh and if you do, please submit a PR for the fixes.
+
 generate_blof_crumb() {
     local args=${@}
 
-    (tar cOPS ${args} --owner=0 <(cat) | gzip -9 | xxd -b -c256 | awk -F": |  |\n" '{print $2}' | sed 's/\s//g' | tr -d '\n') 2>&1
+    (tar cOPS ${args} --transform='s|$|.blof.out|' --sort=name --owner=0 --group=0 --numeric-owner --format=gnu <(cat) | gzip -9 | xxd -b -c256 | awk -F": |  |\n" '{print $2}' | sed 's/\s//g' | tr -d '\n') 2>&1
+
+    return $?
 }
 
 blof() {
-    local BLOF_CRUMB=$(generate_blof_crumb "$@")
-    local bit0=$(echo $BLOF_CRUMB | head -c 1)
+    if [ "$1" = "-x" ] || [ "$1" = "x" ]; then
+        # Extract mode
+        tail -c +1 | fold -w8 | awk '{ printf("%c", strtonum("0b" $0)) }' | gzip -d | tar xOP
 
-    if [ $bit0 != "0" -a $bit0 != "1" ]; then
-        echo "$BLOF_CRUMB" 1>&2
-	return 1
+        return $?
     fi
 
-    printf "%s" "$BLOF_CRUMB"
+    if [ "$1" = "-c" ] || [ "$1" = "c" ]; then
+        shift
 
-    return 0
+        local BLOF_CRUMB=$(generate_blof_crumb "$@")
+        local bit0=$(echo $BLOF_CRUMB | head -c 1)
+
+        if [ $bit0 != "0" -a $bit0 != "1" ]; then
+            echo "$BLOF_CRUMB" 1>&2
+            return 1
+        fi
+
+        printf "%s" "$BLOF_CRUMB"
+
+	return 0
+    fi
+
+    # Default mode
+    "$0" c "$@" < <("$0" c "$@" < "$0")
+    return $?
 }
 
 blof "$@"
